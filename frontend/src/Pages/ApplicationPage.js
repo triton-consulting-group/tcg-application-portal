5812d9df2b3b590e00757a602fb6174e98da045a
himansig7 <himansig7@gmail.com>
2025-02-17 00:49:08 -0800

    Added chakra components to application page

--- frontend/src/Pages/ApplicationPage.js
+++ frontend/src/Pages/ApplicationPage.js
@@ -1,20 +1,71 @@
-import React from "react"
-import { Flex, Input } from "@chakra-ui/react"
+import React from "react";
+import {
+  Flex,
+  VStack,
+  Input,
+  Textarea,
+  Button,
+  Box,
+  Text,
+} from "@chakra-ui/react";
+
+const currentYear = new Date().getFullYear();
+const gradYears = Array.from({ length: 5 }, (_, i) => currentYear + i);
+const minYear = currentYear;
+const maxYear = currentYear + 4;
+const handleYearChange = (e) => {
+  let value = parseInt(e.target.value);
+  if (value < currentYear || value > currentYear + 5) {
+    alert("Please enter a valid graduation year!");
+    e.target.value = "";
+  }
+};
 
 const ApplicationPage = () => {
   return (
-   <Flex align="center" justify="center" direction="column" height="100vh">
-   <div>
-    <h1>User Application Page</h1>
-    <p>Application form will go here.</p>
-    <Input placeholder="First Name" />
-    <Input placeholder="Last Name" />
-    <Input placeholder="Graduation Year" type="number" />
-    <Input placeholder="Email" />
-    <Input placeholder="Major" />
-   </div>
-  </Flex>
-  )
-}
+    <VStack spacing={4} align="stretch" maxW="500px" mx="auto" mt={10}>
+      {/* First Name */}
+      <Box>
+        <Text fontWeight="bold">First Name</Text>
+        <Input placeholder="Enter your first name" />
+      </Box>
 
-export default ApplicationPage
+      {/* Last Name */}
+      <Box>
+        <Text fontWeight="bold">Last Name</Text>
+        <Input placeholder="Enter your last name" />
+      </Box>
+
+      {/* Graduation Year */}
+      <Box>
+        <Text fontWeight="bold">Graduation Year</Text>
+        <Input type="number" onBlur={handleYearChange} placeholder="YYYY" />
+      </Box>
+
+      {/* Major */}
+      <Box>
+        <Text fontWeight="bold">Major</Text>
+        <Input placeholder="Enter your major" />
+      </Box>
+
+      {/* Why do you want to join TCG? */}
+      <Box>
+        <Text fontWeight="bold">Why do you want to join TCG?</Text>
+        <Textarea placeholder="Explain your interest in TCG..." />
+      </Box>
+
+      {/* Upload Resume */}
+      <Box>
+        <Text fontWeight="bold">Upload Resume</Text>
+        <Input type="file" />
+      </Box>
+
+      {/* Submit Button */}
+      <Button colorScheme="blue" size="lg">
+        Submit Application
+      </Button>
+    </VStack>
+  );
+};
+
+export default ApplicationPage;
