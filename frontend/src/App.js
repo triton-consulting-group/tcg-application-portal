de460503146837312b144aabc41809cf44c4ca23
himansig7 <himansig7@gmail.com>
2025-02-27 01:38:27 -0800

    implemented different states for the form (open, submitted, closed)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,22 @@
 import React from "react";
 import { Provider } from "./components/ui/provider";
+import Navbar from "./components/ui/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./Pages/HomePage";
+import HomePage from "./Pages/Homepage";
 import ApplicationPage from "./Pages/ApplicationPage";
-import AssociatePage from "./Pages/AssociatePage";
-import SuccessPage from "./Pages/SuccessPage";
 
 const App = () => {
   return (
-    <Provider> 
-      <Router> 
+    <Provider>
+      <Router>
+        <Navbar />
         <Routes>
-        <Route path="/" element={<HomePage/>} />
-          <Route path="/Application" element={<ApplicationPage />} />
-          {/* 🏠 Home Page Route */}
           <Route path="/" element={<HomePage />} />
-
-          {/* 📄 Application Page Route */}
           <Route path="/application" element={<ApplicationPage />} />
-
-          {/* 👥 Associate Page Route */}
-          <Route path="/associate" element={<AssociatePage />} />
-
-          {/*Success Page Route*/}
-          <Route path="/success" element={<SuccessPage />} />
-
         </Routes>
       </Router>
     </Provider>
   );
-}
+};
 
 export default App;
