798223577fd8e0ea3ab71019af5a064a413d16fe
aprilhuang39 <aprilrouhuang@gmail.com>
2025-10-06 00:36:42 -0700

    fix firebase config

--- frontend/src/Pages/Home/firebaseConfig.js
+++ frontend/src/Pages/Home/firebaseConfig.js
@@ -3,9 +3,9 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDlQkiPSwjY_MTSSliU9mLT62z2LSj74Zo",
-  authDomain: "tcgfirebase-2158a.firebaseapp.com",
-  projectId: "tcgfirebase-2158a",
-  storageBucket: "tcgfirebase-2158a.firebasestorage.app",
+  authDomain: "tcgfirebase-db9b2.firebaseapp.com",
+  projectId: "tcgfirebase-db9b2",
+  storageBucket: "tcgfirebase-db9b2.firebasestorage.app",
   messagingSenderId: "89677566801",
   appId: "1:89677566801:web:cb7b8933c0e3a228bc3970",
   measurementId: "G-VGQWNENVYM"
